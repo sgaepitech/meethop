de54b6bc8bba903ed4ce8519e174c8c6c605fe5f
thibaut <thibaut.lagarde@epitech.eu>
2020-01-29 17:10:20 +0100

    DB connection backend is working on port 5000

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,20 @@
 const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const PORT = 4000;
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT;
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
-mongoose.connect('mongodb+srv://kejoff:jeanmichelmeethop@meethop-eaq2q.gcp.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
-const connection = mongoose.connection;
-
-connection.once('open', function() {
-    console.log("MongoDB database connection established successfully");
+const uri = process.env.ATLAS_URI;mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+const connection = mongoose.connection;connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
 })
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
