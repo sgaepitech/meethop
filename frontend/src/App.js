e98cda88de65056aa94ee1ce2e37a92f66ea440b
quentin.gimfeld <quentin.gimfeld@epitech.eu>
2020-02-03 11:02:04 +0100

    added 3 routes for frontend : /profile /login /register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,16 +7,18 @@ import {
 } from "react-router-dom";
 
 import './App.css';
-import Login from './components/login.form.js';
 
-import UserDashboard from "./components/userDashboard.component"
+import Login from './components/login.form.js';
+import Register from './components/register.form.js';
+import UserProfile from "./components/userProfile.component"
 
 function App() {
   return (
     <Router>
       
-
-      <Route path="/dashboard" component={UserDashboard} />
+      <Route path="/register" component={Register} />
+      <Route path="/login" component={Login} />
+      <Route path="/profile" component={UserProfile} />
     </Router>
   );
 }
