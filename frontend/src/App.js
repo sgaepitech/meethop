c4e67c34853607274571fc8669d4a264d4de262b
thibaut <thibaut.lagarde@epitech.eu>
2020-02-05 18:05:04 +0100

    working routes and navbar, authentication management still not implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link
 } from "react-router-dom";
-
 import './App.css';
 import './css/text.css';
-
-import Register from './components/form/register.form';
-import Login from './components/form/login.form';
 import UserProfile from "./components/userProfile.component"
 import CardMedia  from './components/card.js';
-import ScrollableTabsButtonForce from './components/tabbar.js';
-import landingpage from './components/landingPage.component'
+import landingpage from './components/landingPage.component';
+import NavBar from './components/navbar/navbar.component';
 
-function App() {
-  return (
-    <Router>
-      <Route exact path="/" component={landingpage} />
-      <Route path="/register" component={Register} />
-      <Route path="/login" component={Login} />
-
-
-      <Route path="/logged" component={ScrollableTabsButtonForce} />
-      <Switch>
-
-      <Route path="/logged/test" component={CardMedia} />
-      <Route path="/logged/profile" component={UserProfile} />
-      </Switch>
-    </Router>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+            <NavBar logged="" />
+            <Switch>
+                <Route exact path='/' component={landingpage} />
+                <Route path='/userprofile' component={UserProfile} />
+                <Route path='/eventmanagement' component={CardMedia} />
+            </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
