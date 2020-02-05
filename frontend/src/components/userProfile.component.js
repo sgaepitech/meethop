f8507ca2498d75eab5b8a03ef45ef101491220c0
quentin.gimfeld <quentin.gimfeld@epitech.eu>
2020-02-05 17:54:48 +0100

    Profile page, basic feature with the R part of the CRUD

--- frontend/src/components/userProfile.component.js
+++ frontend/src/components/userProfile.component.js
@@ -46,48 +46,10 @@ const useStyles = makeStyles(theme => ({
 
 const ProfileHeader = () => {
   let classes = useStyles();
-<<<<<<< HEAD
   return (
       <Grid container className={classes.header} direction="column" justify="center" alignItems="center">
         <Grid item xs={12}>
           <Avatar className={classes.avatar} alt={SampleUser.username} src={require("../img/avatar_sample.png")} />
-=======
-  return(
-  <div className={classes.root}>
-      <Paper className={classes.paper}>
-      <Avatar src={require("../img/avatar_sample.png")} className={classes.large} />
-        <Grid container spacing={2}>
-          <Grid item>
-            <ButtonBase className={classes.image}>
-            <AssistantIcon className={classes.customIconSize}/>
-            </ButtonBase>
-          </Grid>
-          <Grid item xs={12} sm container>
-            <Grid item xs container direction="column" spacing={2}>
-              <Grid item xs>
-                <Typography gutterBottom variant="subtitle1">
-                  My personal description
-                </Typography>
-                <Typography variant="body2" gutterBottom>
-                <Avatar src={require("../img/avatar_sample.png")} className={classes.large} />
-                </Typography>
-                <Typography variant="body2" color="textSecondary">
-                  I'm living in {SampleUser.localisation}
-                </Typography>
-                <Typography variant="body2" color="textSecondary">
-                  I was born the {SampleUser.birthdate}
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Typography variant="body2" style={{ cursor: 'pointer' }}>
-                  <Button variant="contained" color="primary">
-                      Edit description
-                  </Button>
-                </Typography>
-              </Grid>
-            </Grid>
-          </Grid>
->>>>>>> 88dc9e7b940d72ac35f5a9151cf349ab03570a22
         </Grid>
       </Grid>
   )
@@ -95,7 +57,6 @@ const ProfileHeader = () => {
 
 const ProfileInformations = () => {
   let classes = useStyles();
-<<<<<<< HEAD
   return (
       <Grid container spacing={3} justify="center">
         <Grid item xs={6}>
@@ -114,110 +75,11 @@ const UserInterests = () => {
       <Grid container className={classes.gridContain}>
         <Grid item xs={12}>
           <MeethopCard type="userCard" title="User Interests" interests={SampleUser.interests}/>
-=======
-  return(
-  <div className={classes.root}>
-
-      <Paper className={classes.paper}>
-        <Grid container spacing={2}>
-        <Avatar src={require("../img/avatar_sample.png")} className={classes.large} />
-
-          <Grid item>
-            <ButtonBase className={classes.image}>
-            <AssistantIcon className={classes.customIconSize}/>
-            </ButtonBase>
-          </Grid>
-          <Grid item xs={12} sm container>
-            <Grid item xs container direction="column" spacing={2}>
-              <Grid item xs>
-                <Typography gutterBottom variant="subtitle1">
-                  My personal description
-                </Typography>
-                <Typography variant="body2" gutterBottom>
-                  {SampleUser.description}
-                </Typography>
-                <Typography variant="body2" color="textSecondary">
-                  I'm living in {SampleUser.localisation}
-                </Typography>
-                <Typography variant="body2" color="textSecondary">
-                  I was born the {SampleUser.birthdate}
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Typography variant="body2" style={{ cursor: 'pointer' }}>
-                  <Button variant="contained" color="primary">
-                      Edit description
-                  </Button>
-                </Typography>
-              </Grid>
-            </Grid>
-          </Grid>
-        </Grid>
-      </Paper>
-    </div>
-  )}
-
-const ChipsArray = () => {
-    const classes = useStyles();
-    var myInterests = SampleUser.interests.map((obj, i) => {
-      var rObj = {};
-      rObj.key = i;
-      rObj.label = obj;
-      return rObj;
-    });
-
-    const [chipData, setChipData] = React.useState(myInterests);
-
-    const handleDelete = chipToDelete => () => {
-      setChipData(chips => chips.filter(chip => chip.key !== chipToDelete.key));
-    };
-
-    return (
-      <div>
-        {chipData.map(data => {
-          return (
-            <Chip
-              variant="outlined"
-              color="primary"
-              key={data.key}
-              label={data.label}
-              onDelete={data.label === 'React' ? undefined : handleDelete(data)}//if label has a specific name, no deletion option
-              className={classes.chip}
-            />
-          );
-        })}
-      </div>
-    );
-  }
-
-const UserInterests = () => {
-  let classes = useStyles();
-  return(
-  <div className={classes.root}>
-      <Paper className={classes.paper}>
-        <Grid container spacing={2}>
-          <Grid item>
-          <ButtonBase className={classes.image}>
-            <LocalActivityIcon className={classes.customIconSize}/>
-          </ButtonBase>
-          </Grid>
-          <Grid item xs={12} sm container>
-            <Grid item xs container direction="column" spacing={2}>
-              <Grid item xs>
-                <Typography gutterBottom variant="subtitle1">
-                  My personal interests
-                </Typography>
-                <ChipsArray/>
-              </Grid>
-            </Grid>
-          </Grid>
->>>>>>> 88dc9e7b940d72ac35f5a9151cf349ab03570a22
         </Grid>
       </Grid>
   )
 }
 
-<<<<<<< HEAD
 export default class UserProfile extends Component {
 
   render() {
@@ -231,16 +93,3 @@ export default class UserProfile extends Component {
     )
   }
 }
-=======
-  export default class UserDashboard extends Component {
-
-    render () {
-      return(
-        <div className="container">
-          <UserDescription/>
-          <UserInterests/>
-        </div>
-        )
-      }
-}
->>>>>>> 88dc9e7b940d72ac35f5a9151cf349ab03570a22
